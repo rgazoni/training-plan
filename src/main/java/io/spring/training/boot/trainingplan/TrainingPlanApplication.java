594d5ce2711f36758a92f63218336e2af4b6471f
rgazoni <ramongazonil@gmail.com>
2025-08-11 11:10:29 -0300

    initial commit

---
+++ src/main/java/io/spring/training/boot/trainingplan/TrainingPlanApplication.java
@@ -0,0 +1,13 @@
+package io.spring.training.boot.trainingplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/spring/training/boot/trainingplan/TrainingPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.training.boot.trainingplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
