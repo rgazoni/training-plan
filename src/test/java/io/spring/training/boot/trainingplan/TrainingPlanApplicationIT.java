97b4c4d9566a0a7c5cd72c181d49e74bf434812b
rgazoni <ramongazonil@gmail.com>
2025-08-20 15:16:45 -0300

    feat: creating structure for tests, with TestContainers, AssertJ, Mockito, JUnit5

---
+++ src/test/java/io/spring/training/boot/trainingplan/TrainingPlanApplicationIT.java
@@ -0,0 +1,34 @@
+package io.spring.training.boot.trainingplan;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Tag("integration")
+class TrainingPlanApplicationIT {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    ).withReuse(true);
+
+    @DynamicPropertySource
+    static void dbProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", postgres::getJdbcUrl);
+        r.add("spring.datasource.username", postgres::getUsername);
+        r.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void sanity(){}
+
+}
