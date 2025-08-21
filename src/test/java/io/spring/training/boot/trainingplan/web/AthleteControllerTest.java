eb9f2edca1ab1954482b23b3829caf0a0794996c
rgazoni <ramongazonil@gmail.com>
2025-08-21 16:11:05 -0300

    feat: Created a test to check if the CPF is valid for athlete controller upon creation

--- src/test/java/io/spring/training/boot/trainingplan/web/AthleteControllerTest.java
+++ src/test/java/io/spring/training/boot/trainingplan/web/AthleteControllerTest.java
@@ -3,13 +3,18 @@ package io.spring.training.boot.trainingplan.web;
 import io.spring.training.boot.trainingplan.domain.Athlete;
 import io.spring.training.boot.trainingplan.service.AthleteService;
 import io.spring.training.boot.trainingplan.web.mapper.AthleteMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -17,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @WebMvcTest(AthleteController.class)
 public class AthleteControllerTest {
 
@@ -59,6 +65,33 @@ public class AthleteControllerTest {
                     }
                  """))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createAthlete_returns400_onInvalidCPF() throws Exception {
+        mvc.perform(post("/athletes/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                            "cpf": "156$c",
+                            "weightKg": 70.5,
+                            "age": 31,
+                            "firstName": "Michael",
+                            "lastName": "Scott"
+                    }
+                 """))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> {
+                    Exception ex = result.getResolvedException();
+                    assertThat(ex).isInstanceOf(MethodArgumentNotValidException.class);
+                    var manve = (MethodArgumentNotValidException) ex;
+                    assert manve != null;
+                    var msg = Objects.requireNonNull(manve.getBindingResult()
+                                    .getFieldError("cpf"))
+                            .getDefaultMessage();
+                    assertThat(msg).contains("invalid Brazilian");
+                });
+
     }
 
 }
